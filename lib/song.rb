3cbaea8a2e04377d612822dc6741f8ef1e1ee457
Nestor <nestor.venegas95@gmail.com>
2021-02-25 22:19:43 -0600

    commit tree 28197cadd68bc563088939d16079fe27166f3f13

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,57 @@ class Song
   def self.all
     @@all
   end
-
+  
   def save
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  # def name
+  #   @name
+  # end
+
+  # def artist_name
+  #   @artist_name
+  # end
+
+  def self.new_by_name(name)
+      song = self.new
+      song.name = name
+      song
+  end
+
+  def self.create_by_name(name)
+      song = self.create
+      song.name = name
+      song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+   #   def self.new_from_filename(filename)
+   #    song = self.new
+   #    artist = song.artist_name
+   #    artist.remove(0).
+   #    song = "#{artist}" + "-" + "#{filename}"
+   #   end
+  
+   def self.destroy_all
+       self.all.clear
+   end
 end
